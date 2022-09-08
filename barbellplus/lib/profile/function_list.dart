a6c7d8091d40d51fdd1824efb887869710675afe
Leonard Marshall Afzal <loma256@gmail.com>
2022-09-08 19:55:12 +0100

    Implement function list for profile screen

--- barbellplus/lib/profile/function_list.dart
+++ barbellplus/lib/profile/function_list.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class FunctionList extends StatelessWidget {
@@ -5,11 +7,61 @@ class FunctionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      width: double.infinity,
-      height: 276,
-      child: const Text('Function List'),
+    return Expanded(
+      child: DecoratedBox(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) => const FunctionItem(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FunctionItem extends StatelessWidget {
+  const FunctionItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/exercise');
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 5),
+        height: 30,
+        decoration: BoxDecoration(
+          color: Colors.black12,
+          borderRadius: BorderRadius.circular(3),
+        ),
+        child: Row(
+          children: const [
+            SizedBox(
+                width: 50,
+                height: 50,
+                child: Icon(Icons.add_box_outlined, size: 20)),
+            SizedBox(width: 30),
+            Text('Some function or ability',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
     );
   }
 }
